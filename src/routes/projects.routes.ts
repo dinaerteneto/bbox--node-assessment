7eb497833310377543bcbad20bf40de94ebfc17a
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-05 02:04:06 -0300

    feat: add route validation

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -3,10 +3,19 @@ import { createProjectController } from '../useCases/projects/CreateProject'
 import { deleteProjectController } from '../useCases/projects/DeleteProject'
 import { getProjectController } from '../useCases/projects/GetProject'
 import { getProjectsController } from '../useCases/projects/GetProjects'
+import { yup, validate } from './validator';
 
 const projectsRouter = Router()
 
 projectsRouter.post('/', async (request, response) => {
+    const schema = yup.object().shape({
+        description: yup.string().required(),
+        userId: yup.string().required()
+    });
+    const errors = await validate(schema, request.body)
+    if (errors) {
+        return response.json(errors)
+    }        
     return createProjectController.handle(request, response)
 })
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,9 +4,23 @@ import { deleteUserController } from '../useCases/users/DeleteUser'
 import { getUsersController } from '../useCases/users/GetUsers'
 import { getUserController } from '../useCases/users/GetUser'
 
+import { yup, validate } from './validator';
+
 const usersRouter = Router()
 
 usersRouter.post('/', async (request, response) => {
+    const schema = yup.object().shape({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        email: yup.string().email().required(),
+        password: yup.string().required(),
+        phoneNumber: yup.string()
+    });
+    const errors = await validate(schema, request.body)
+    if (errors) {
+        return response.json(errors)
+    }    
+
     return createUserController.handle(request, response)
 })
 
