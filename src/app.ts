7d1992e76439d61edb02299e90fbabe8d76c51db
Dinaerte Neto <dinaerteneto@gmail.com>
2021-05-04 23:05:05 -0300

    feat: separating boot into files

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { AppError } from './errors/AppError';
+import { router } from './routes'
+import './database';
+
+const app = express()
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response
+        .status(err.statusCode)
+        .json({ status: 'error', message: err.message });
+    }
+  
+    console.error(err);
+  
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error' });
+  });
+
+app.use(express.json())
+app.use(router)
+
+export { app }
